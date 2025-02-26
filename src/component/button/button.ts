66af7a1a9e0d87bf31d85d21117dcc609b17b926
Philip Weinke <its@weph.dev>
2025-02-26 08:19:40 +0100

    Convert raw button component to lit component

--- src/component/button/button.ts
+++ src/component/button/button.ts
@@ -1,62 +1,36 @@
-export interface ButtonProps {
-    text: string
-}
-
-export class Button extends HTMLElement implements ButtonProps {
-    private root: ShadowRoot
-
-    constructor() {
-        super()
-
-        this.root = this.attachShadow({ mode: 'open' })
-
-        this.update()
-    }
-
-    private update(): void {
-        this.root.innerHTML = `
-            <button>${this.text}</button>
-            <style>
-                button {
-                    display: block;
-                    appearance: none;
-                    height: var(--bookshelf--ui--button--height);
-                    border-radius: var(--bookshelf--ui--button--border-radius);
-                    border-width: var(--bookshelf--ui--button--border-width);
-                    box-shadow: var(--bookshelf--ui--button--box-shadow);
-                    font-weight: var(--bookshelf--ui--button--font-weight);
-                    background-color: var(--bookshelf--ui--button--background);
-                    padding: var(--bookshelf--ui--button--padding);
-                }
-                
-                button:hover {
-                    background-color: var(--bookshelf--ui--button--hover--background);
-                    box-shadow: var(--bookshelf--ui--button--hover--box-shadow);
-                }            
-            </style>
-        `
-    }
-
-    static get observedAttributes() {
-        return ['text']
-    }
-
-    public attributeChangedCallback(): void {
-        this.update()
-    }
-
-    get text(): string {
-        return this.getAttribute('text') || ''
-    }
-
-    set text(value: string) {
-        this.setAttribute('text', value)
-    }
-}
+import { css, html, LitElement } from 'lit'
+import { customElement, property } from 'lit/decorators.js'
 
 const TAG_NAME = 'bookshelf-ui-button'
 
-customElements.define(TAG_NAME, Button)
+@customElement(TAG_NAME)
+export class Button extends LitElement {
+    static styles = css`
+        button {
+            display: block;
+            appearance: none;
+            height: var(--bookshelf--ui--button--height);
+            border-radius: var(--bookshelf--ui--button--border-radius);
+            border-width: var(--bookshelf--ui--button--border-width);
+            box-shadow: var(--bookshelf--ui--button--box-shadow);
+            font-weight: var(--bookshelf--ui--button--font-weight);
+            background-color: var(--bookshelf--ui--button--background);
+            padding: var(--bookshelf--ui--button--padding);
+        }
+
+        button:hover {
+            background-color: var(--bookshelf--ui--button--hover--background);
+            box-shadow: var(--bookshelf--ui--button--hover--box-shadow);
+        }
+    `
+
+    @property()
+    public text: string
+
+    protected render() {
+        return html` <button>${this.text}</button>`
+    }
+}
 
 declare global {
     interface HTMLElementTagNameMap {
