54567db0a1e3f2c8d2111de26ab318cdcc81c6d7
Philip Weinke <its@weph.dev>
2025-02-26 06:06:12 +0100

    Convert raw star rating web component to lit component

--- src/component/star-rating/star-rating.ts
+++ src/component/star-rating/star-rating.ts
@@ -1,87 +1,62 @@
-export interface StarRatingProps {
-    value?: number
-}
+import { css, html, LitElement } from 'lit'
+import { customElement, property } from 'lit/decorators.js'
 
-export class StarRating extends HTMLElement implements StarRatingProps {
-    private root: ShadowRoot
+const TAG_NAME = 'bookshelf-ui-star-rating'
 
-    private starContainer: HTMLElement
+@customElement(TAG_NAME)
+export class StarRating extends LitElement {
+    static styles = css`
+        main {
+            display: flex;
+            flex-direction: row;
+            flex-wrap: nowrap;
+            align-items: center;
+        }
+
+        .star {
+            overflow: hidden;
+        }
+    `
 
     private iconSize = 15
 
-    constructor() {
-        super()
+    @property({ type: Number })
+    public value: number = 0
 
-        this.root = this.attachShadow({ mode: 'open' })
-        this.root.innerHTML = `
-            <main></main>
-            <style>
-                main {
-                    display: flex;
-                    flex-direction: row;
-                    flex-wrap: nowrap;
-                    align-items: center;
-                }
-                
-                .star {
-                    overflow: hidden;
-                }
-            </style>
-        `
-
-        this.starContainer = this.root.querySelector('main')!
-    }
-
-    static get observedAttributes() {
-        return ['value']
-    }
-
-    public attributeChangedCallback(): void {
-        this.update()
-    }
-
-    private update(): void {
-        if (this.value === undefined) {
-            this.starContainer.innerHTML = ''
-            return
-        }
-
+    protected render() {
         const full = Math.trunc(this.value)
         const fraction = this.value - full
 
-        this.starContainer.innerHTML = Array(full).fill(this.star(1)).join('') + this.star(fraction)
+        return html`<main>${Array(full).fill(this.star(1))} ${this.star(fraction)}</main>`
     }
 
-    private star(percentage: number): string {
+    private star(percentage: number) {
         if (percentage === 0) {
             return ''
         }
 
-        return `
+        return html`
             <div class="star" style="width: ${this.iconSize * percentage}px; height: ${this.iconSize}px">
-                <svg xmlns="http://www.w3.org/2000/svg" width="${this.iconSize}" height="${this.iconSize}" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-star"><path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"/></svg>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="${this.iconSize}"
+                    height="${this.iconSize}"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="lucide lucide-star"
+                >
+                    <path
+                        d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"
+                    />
+                </svg>
             </div>
         `
     }
-
-    set value(value: number) {
-        this.setAttribute('value', value.toString())
-    }
-
-    get value(): number | undefined {
-        const attrValue = this.getAttribute('value')
-
-        if (attrValue === null) {
-            return undefined
-        }
-
-        return parseFloat(attrValue)
-    }
 }
-
-const TAG_NAME = 'bookshelf-ui-star-rating'
-
-customElements.define(TAG_NAME, StarRating)
 
 declare global {
     interface HTMLElementTagNameMap {
