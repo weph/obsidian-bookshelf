ea0919d7a24b4e4209dbbc76b5f5aa0ff5f5995a
Philip Weinke <its@weph.dev>
2025-02-27 07:22:16 +0100

    Convert raw gallery component to lit component

--- src/component/gallery/gallery.ts
+++ src/component/gallery/gallery.ts
@@ -1,71 +1,43 @@
 import { Book } from '../../bookshelf/book'
 import './gallery-card'
-import { GalleryCard } from './gallery-card'
-
-export interface GalleryProps {
-    books: Array<Book>
-    onBookClick: ((book: Book) => void) | null
-}
-
-export class Gallery extends HTMLElement implements GalleryProps {
-    private root: ShadowRoot
-
-    private _books: Array<Book> = []
-
-    public onBookClick: ((book: Book) => void) | null = null
-
-    constructor() {
-        super()
-
-        this.root = this.attachShadow({ mode: 'open' })
-        this.root.addEventListener('click', (e) => {
-            if (this.onBookClick === null) {
-                return
-            }
-
-            const index = (e.target as GalleryCard).getAttribute('index') || ''
-            this.onBookClick(this._books[parseInt(index)])
-        })
-    }
-
-    private update() {
-        const markup = this._books
-            .map(
-                (book, index) => `<div role="list">
-                    <bookshelf-gallery-card 
-                        index="${index}"
-                        title="${book.metadata.title}"
-                        ${book.metadata.cover ? `cover="${book.metadata.cover}"` : ''}
-                        role="listitem">
-                    </bookshelf-gallery-card>
-                </div>`,
-            )
-            .join('')
-
-        this.root.innerHTML = `${markup}${css()}`
-    }
-
-    set books(value: Array<Book>) {
-        this._books = value
-        this.update()
-    }
-}
-
-function css(): string {
-    return `
-        <style>
-            :host {
-                display: grid;
-                grid-template-columns: repeat(auto-fit, 150px);
-                grid-gap: 15px;
-                justify-content: center;
-            }
-        </style>`
-}
+import { css, html, LitElement } from 'lit'
+import { customElement, property } from 'lit/decorators.js'
 
 const TAG_NAME = 'bookshelf-gallery'
 
-customElements.define(TAG_NAME, Gallery)
+@customElement(TAG_NAME)
+export class Gallery extends LitElement {
+    static styles = css`
+        :host {
+            display: grid;
+            grid-template-columns: repeat(auto-fit, 150px);
+            grid-gap: 15px;
+            justify-content: center;
+        }
+    `
+
+    @property()
+    public books: Array<Book>
+
+    @property()
+    public onBookClick: ((book: Book) => void) | null = null
+
+    protected render() {
+        return html` ${this.books.map(
+            (book, index) =>
+                html` <div role="list">
+                    <bookshelf-gallery-card
+                        index="${index}"
+                        title="${book.metadata.title}"
+                        cover="${book.metadata.cover}"
+                        role="listitem"
+                        @click="${() => this.onBookClick && this.onBookClick(book)}"
+                    >
+                    </bookshelf-gallery-card>
+                </div>`,
+        )}`
+    }
+}
 
 declare global {
     interface HTMLElementTagNameMap {
