8dae1726a37af5f4050eb1338c90dcc74c6497c5
Philip Weinke <its@weph.dev>
2025-03-19 07:53:31 +0100

    Inline chart component as it had only one usage

--- src/component/statistics/pages-read-chart/pages-read-chart.tsx
+++ src/component/statistics/pages-read-chart/pages-read-chart.tsx
@@ -2,8 +2,8 @@ import { Chart, TimeSeriesScale, TimeUnit } from 'chart.js/auto'
 import 'chartjs-adapter-luxon'
 import { Interval, Statistics } from '../../../bookshelf/reading-journey/statistics/statistics'
 import { Dropdown } from '../../dropdown/dropdown'
-import { PagesReadBarChart } from '../../chart/pages-read-bar-chart/pages-read-bar-chart'
 import { useState } from 'react'
+import { Bar } from 'react-chartjs-2'
 
 interface IntervalValue {
     chart: TimeUnit
@@ -57,19 +57,46 @@ Chart.register(TimeSeriesScale)
 export function PagesReadChart({ statistics }: Props) {
     const [interval, setInterval] = useState('year')
 
-    const data = () => {
-        return Array.from(statistics.pagesRead(intervals[interval].statistics).entries()).map((entry) => ({
-            x: entry[0].getTime(),
-            y: entry[1],
-        }))
-    }
+    const data = Array.from(statistics.pagesRead(intervals[interval].statistics).entries()).map((entry) => ({
+        x: entry[0].getTime(),
+        y: entry[1],
+    }))
 
     return (
         <>
             <Dropdown label="Interval" value={interval} options={intervalOptions} onChange={setInterval} />
 
             <div style={{ width: '100%', aspectRatio: '2/1' }}>
-                <PagesReadBarChart data={data()} xAxisUnit={intervals[interval].chart} />
+                <Bar
+                    data={{
+                        datasets: [
+                            {
+                                label: '# of pages',
+                                data,
+                            },
+                        ],
+                    }}
+                    options={{
+                        responsive: true,
+                        maintainAspectRatio: true,
+                        plugins: {
+                            legend: {
+                                display: false,
+                            },
+                        },
+                        scales: {
+                            x: {
+                                type: 'timeseries',
+                                time: {
+                                    unit: intervals[interval].chart,
+                                },
+                            },
+                            y: {
+                                beginAtZero: true,
+                            },
+                        },
+                    }}
+                />
             </div>
         </>
     )
